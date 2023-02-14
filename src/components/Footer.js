5000d0bfb9c9b9d053dfbc136f865f0a2e330ab1
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-14 09:04:58 +0100

    création du footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logoFooter from "../assets/Images/logoFooter.png";
+const Footer = () => {
+  return (
+    <footer>
+      <img src={logoFooter} alt="logo de Kasa" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
