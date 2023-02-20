9efa865d08b54072895f261bed8f6817cb9ad776
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-20 09:21:09 +0100

    route vers la bannière

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BannerImg from "../components/BannerImg";
 import Footer from "../components/Footer";
 import Logo from "../components/Header";
 
@@ -6,6 +7,9 @@ const About = () => {
   return (
     <div>
       <Logo />
+      <main>
+        <BannerImg />
+      </main>
       <Footer />
     </div>
   );
