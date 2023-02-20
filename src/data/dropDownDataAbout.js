1fd770b710f1475f3f5359be4ae86ffe9a6cb1bb
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-20 15:16:03 +0100

    tableau dropdown About

---
+++ src/data/dropDownDataAbout.js
@@ -0,0 +1,22 @@
+export const dropDownData = [
+  {
+    title: "Fiabilité",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur assumenda porro asperiores corporis amet soluta dicta, eligendi consequuntur in laudantium omnis, culpa minima, accusantium atque sit facere quisquam fugiat! Repudiandae tenetur nesciunt deserunt totam illum doloremque sed quisquam harum quae?",
+  },
+  {
+    title: "Respect",
+    paragraph:
+      "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+  },
+  {
+    title: "Service",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur assumenda porro asperiores corporis amet soluta dicta, eligendi consequuntur in laudantium omnis, culpa minima, accusantium atque sit facere quisquam fugiat! Repudiandae tenetur nesciunt deserunt totam illum doloremque sed quisquam harum quae?",
+  },
+  {
+    title: "Responsabilité",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur assumenda porro asperiores corporis amet soluta dicta, eligendi consequuntur in laudantium omnis, culpa minima, accusantium atque sit facere quisquam fugiat! Repudiandae tenetur nesciunt deserunt totam illum doloremque sed quisquam harum quae?",
+  },
+];
