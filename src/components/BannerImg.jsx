ec5d401005b16545cfcb12c5bcd00c80a72896a7
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-20 10:07:46 +0100

    ajout condition pour changer d'image de banière selon la page

--- src/components/BannerImg.jsx
+++ src/components/BannerImg.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import img from "../assets/Images/Banner.png";
+import { useLocation } from "react-router-dom";
+import imgHome from "../assets/Images/Banner.png";
+import imgAbout from "../assets/Images/aboutImg.png";
+
 const BannerImg = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+  const showTitle = isHomePage;
+
   return (
     <div className="bannerHome bannerHome__overlay">
-      <img src={img} alt="Banner_image" />
-      <h1 className="mainTitle">Chez vous, partout et ailleurs</h1>
+      <img src={isHomePage ? imgHome : imgAbout} alt="Banner_image" />
+      {showTitle && (
+        <h1 className="mainTitle">Chez vous, partout et ailleurs</h1>
+      )}
     </div>
   );
 };
