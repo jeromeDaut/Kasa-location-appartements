f1131c1ada9707f48bacce1f5fe0705526506b2b
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-21 10:55:24 +0100

    routes de navLink

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -4,35 +4,25 @@ import { data } from "../data/logements";
 import arrowLeft from "../assets/Images/arrow_back.png";
 import arrowRight from "../assets/Images/arrow_right.png";
 
-const Carousel = () => {
-  //Declare state variables for the current indes and route
+const Slideshow = () => {
+  //Declare state variables for the current index
   const [index, setIndex] = useState(0);
-  const [route, setRoute] = useState(`/accommodation/${data.id}`);
   const length = data.length;
   //function to go to the previous index
   const CoverPrevious = () => {
     const newIndex = index - 1;
-    const newRoute = `/accommodation/${
-      data[newIndex < 0 ? length - 1 : newIndex].id
-    }`;
-
     setIndex(newIndex < 0 ? length - 1 : newIndex); //Set new index,looping to the end
-    setRoute(newRoute); //Set the new route
   };
 
   //function to go to the next index
   const CoverNext = () => {
     const newIndex = index + 1;
-    const newRoute = `/accommodation/${
-      data[newIndex >= length ? 0 : newIndex].id
-    }`;
     setIndex(newIndex >= length ? 0 : newIndex);
-    setRoute(newRoute);
   };
 
   return (
     <div className="carousel">
-      <NavLink to={route}>
+      <NavLink to={/accommodation/ + data[index].id}>
         <img
           src={arrowLeft}
           alt="fleche location précédente"
@@ -42,7 +32,7 @@ const Carousel = () => {
         />
       </NavLink>
 
-      <NavLink to={route}>
+      <NavLink to={/accommodation/ + data[index].id}>
         <img
           src={arrowRight}
           alt="fleche location suivante"
@@ -50,10 +40,11 @@ const Carousel = () => {
           className="arrowRight"
         />
       </NavLink>
-      <img src={data[index].cover} alt={data.title} />
-      <p>{index + 1 + "/" + data.length}</p>
+
+      <img key={data[index].id} src={data[index].cover} alt={data.title} />
+      <p>{index + 1 + "/" + length}</p>
     </div>
   );
 };
 
-export default Carousel;
+export default Slideshow;
