ae543eb0775651f5b6a486162910a45cba237123
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-22 11:40:32 +0100

    ajout du header et du footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import About from "./pages/About";
 import Accommodation from "./pages/Accommodation";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import Footer from "../src/components/Footer";
+import Header from "../src/components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="/accommodation/:id" element={<Accommodation />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
