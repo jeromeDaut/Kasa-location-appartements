894922830bfcdfeca7f783327deb3000155d5d16
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-22 11:43:07 +0100

    condition pour affichage dropDown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -2,27 +2,28 @@ import React, { useState } from "react";
 import arrowUp from "../assets/Images/arrow_up.png";
 import arrowDown from "../assets/Images/arrow_down.png";
 
-const DropDown = ({ title, paragraph }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const DropDown = ({ title, content, isList = false }) => {
+  const [isOpen, setIsOpen] = useState(true);
 
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <section>
-      <article className="dropDown">
-        <div className="dropDown__header" onClick={toggleOpen}>
-          <h3 className="dropDown__title">{title}</h3>
-          {isOpen ? (
-            <img src={arrowUp} alt="fermé" />
-          ) : (
-            <img src={arrowDown} alt="ouvert" />
-          )}
-        </div>
-        {isOpen && <p className="dropDown__content">{paragraph}</p>}
-      </article>
-    </section>
+    <article className="dropDown">
+      <div className="dropDown__header" onClick={toggleOpen}>
+        <h3 className="dropDown__title">{title}</h3>
+        {isOpen ? (
+          <img src={arrowUp} alt="fermé" />
+        ) : (
+          <img src={arrowDown} alt="ouvert" />
+        )}
+      </div>
+      {/* {isOpen && <p className="dropDown__content">{paragraph}</p>} */}
+      {isList === true
+        ? isOpen && <ul className="dropDown__content">{content}</ul>
+        : isOpen && <p className="dropDown__content">{content}</p>}
+    </article>
   );
 };
 
