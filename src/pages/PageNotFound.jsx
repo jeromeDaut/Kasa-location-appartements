08098fcbf8c578b7c3cdcac63db4f486ba0c15b4
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-22 11:47:08 +0100

    suppression du header/footer

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Logo from "../components/Header";
 import { NavLink } from "react-router-dom";
 const PageNotFound = () => {
   return (
-    <div>
-      <Logo />
-      <div className="error">
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <NavLink to="/">Retourner sur la page d'accueil</NavLink>
-      </div>
-    </div>
+    <main className="error">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+    </main>
   );
 };
 
