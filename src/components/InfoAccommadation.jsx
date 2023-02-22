a1f1b52bfa95b831e3d65a37e1090f4c6ed58108
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-22 11:44:11 +0100

    ajout rating et host

--- src/components/InfoAccommadation.jsx
+++ src/components/InfoAccommadation.jsx
@@ -1,23 +1,88 @@
-import React from "react";
-import { data } from "../data/logements";
+// import React from "react";
+// import starEmpty from "../assets/Images/starEmpty.svg"
+// // import starEmpty from "../assets/Images/starEmpty.svg";
+// // import starFilled from "../assets/Images/starFilled.svg";
+// const InfoAccommadation = ({ title, location, tags, name, picture}) => {
+//   return (
+//     <section>
+//       <div className="info">
+//         <h3 className="info__title">{title}</h3>
+//         <p className="info__location">{location}</p>
+//         <div className="info__tags">
+//           {tags.map((tag) => (
+//             <span key={tag} className="info__tag">
+//               {tag}
+//             </span>
+//           ))}
+//         </div>
+//         <div className="host">
+//           <span>{name}</span>
+//           <img src={picture} alt={name} />
+//         </div>
+//         <div className="rating">
 
-const InfoAccommadation = () => {
-  // const [index, setIndex] = useState(0);
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default InfoAccommadation;
+import React from "react";
+import starEmpty from "../assets/Images/starEmpty.svg";
+import starFilled from "../assets/Images/starFilled.svg";
+
+const InfoAccommadation = ({
+  title,
+  location,
+  tags,
+  name,
+  picture,
+  rating,
+}) => {
+  // RATING:
+  // transform string in number:
+  const ratingNum = parseInt(rating);
+
+  // Array with the number of stars corresponding to the rating
+  const filledStars = Array.from({ length: ratingNum }, (_, i) => i);
+
+  // Array with the remaining stars
+  const emptyStars = Array.from(
+    { length: 5 - ratingNum },
+    (_, i) => i + ratingNum
+  );
+
   return (
-    <section>
-      {data.map((item) => (
-        <div className="info" key={item.id}>
-          <h3 className="info__title">{item.title}</h3>
-          <p className="info__location">{item.location}</p>
-          <div className="info__tags">
-            {item.tags.map((tag) => (
-              <span key={tag} className="info__tag">
-                {tag}
-              </span>
-            ))}
-          </div>
+    <section className="sectionInfo">
+      <div className="info">
+        <h3 className="info__title">{title}</h3>
+        <p className="info__location">{location}</p>
+        {/* tags */}
+        <div className="info__tags">
+          {tags.map((tag) => (
+            <span key={tag} className="info__tag">
+              {tag}
+            </span>
+          ))}
         </div>
-      ))}
+      </div>
+      <div className="containerHost">
+        {/* host */}
+        <div className="host">
+          <h3>{name}</h3>
+          <img src={picture} alt={name} />
+        </div>
+        {/* rating */}
+        <div className="rating">
+          {filledStars.map((star) => (
+            <img key={star} src={starFilled} alt="Filled Star" />
+          ))}
+          {emptyStars.map((star) => (
+            <img key={star} src={starEmpty} alt="Empty Star" />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
