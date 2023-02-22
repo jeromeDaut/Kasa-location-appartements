13176d41bf6fe7ffbb1e8edfc1c2b8dea1471973
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-22 11:43:45 +0100

    ajout state

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,8 +5,13 @@ import { data } from "../data/logements";
 const Gallery = () => {
   return (
     <section className="gallery">
-      {data.map((item) => (
-        <NavLink to={/accommodation/ + item.id} className="card" key={item.id}>
+      {data.map((item, index) => (
+        <NavLink
+          to={/accommodation/ + item.id}
+          state={{ logement: data[index] }}
+          className="card"
+          key={item.id}
+        >
           <img src={item.cover} alt={item.title} />
           <h3>{item.title}</h3>
         </NavLink>
