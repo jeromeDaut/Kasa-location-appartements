ef02b1b31f268f2403ed4aef5087f0ab880b3cfc
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-02-22 11:46:43 +0100

    suppression du header/footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import BannerImg from "../components/BannerImg";
-import Footer from "../components/Footer";
 import Gallery from "../components/Gallery";
-import Logo from "../components/Header";
 
 const Home = () => {
   return (
-    <>
-      <Logo />
-      <main>
-        <BannerImg />
-        <Gallery />
-      </main>
-      <Footer />
-    </>
+    <main>
+      <BannerImg />
+      <Gallery />
+    </main>
   );
 };
 
